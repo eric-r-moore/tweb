5bbbd5536921b0672ddefb9a4bc707b3b2df6f56
Eric Moore <EricGittaMoore@duck.com>
2024-11-12 09:10:22 +0000

    hide

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,8 @@ const rootDir = resolve(__dirname);
 
 const handlebarsPlugin = handlebars({
   context: {
-    title: 'Telegram Web',
-    description: 'Telegram is a cloud-based mobile and desktop messaging app with a focus on security and speed.',
-    url: 'https://web.telegram.org/k/',
-    origin: 'https://web.telegram.org/'
+    title: 'Cloud Shell',
+    description: 'Cloud Shell',
   }
 });
 
